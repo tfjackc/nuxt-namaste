f60a79ad2a6531efb97f9bf975ea6c58c2f12c15
tfjackc <jack.colpitt@crookcountyor.gov>
2023-12-07 11:50:32 -0800

    trying new adjustments

--- pages/namaste.vue
+++ pages/namaste.vue
@@ -9,9 +9,11 @@
 
 
   <div class="top-div d-flex justify-center">
-  <v-col cols="12" md="8" class="mx-auto">
-  <v-card class="pb-5 pt-5 shadow-lg rounded"
-           image="~/assets/hawaii_sunset.jpg">
+
+  <v-img
+          src="~/assets/oceanView.jpg"
+         aspect-ratio="16/9"
+         cover>
     <v-container class="py-5 text-center">
       <v-row class="py-lg-5">
         <v-col cols="12" md="8" lg="6" class="mx-auto">
@@ -68,69 +70,76 @@
         </v-col>
       </v-row>
     </v-container>
-  </v-card>
-    </v-col>
-  </div>
-    </v-row>
     <v-row>
-  <div class="bottom-div d-flex justify-center">
-       <v-card class="pb-5 pt-5 shadow-lg rounded bottom-card"
-      image="~/assets/oceanView.jpg">
-         <v-container class="py-5 text-center">
-           <v-carousel hide-delimiters>
-             <v-carousel-item>
-                 <v-card color="transparent"
-                 elevation="10">
+      <div class="bottom-div d-flex justify-center">
+        <v-card class="pb-5 pt-5 shadow-lg rounded bottom-card"
+        >
+          <v-container class="py-5 text-center shadow-lg">
+            <v-card class="pb-5 pt-5 shadow-lg rounded"
+                    color="grey-lighten-5"
+                    elevation="20">
+              <v-window  show-arrows>
+                <v-window-item>
+                  <v-card color="transparent"
+                          elevation="10">
                     <v-card-title
-                        >New Year Yoga - You Are Awesome!</v-card-title>
+                    >Daylight Savings Time (Yoga for Morning)</v-card-title>
+                    <v-card-text>
+                      <video controls width="600" poster="https://herokustatic.s3.us-west-2.amazonaws.com/vid3_ss.png">
+                        <source src="https://herokustatic.s3.us-west-2.amazonaws.com/Daylight_Savings.mp4" type="video/mp4" />
+                      </video>
+                    </v-card-text>
+                  </v-card>
+                </v-window-item>
+                <v-window-item>
+                  <v-card color="transparent"
+                          elevation="10">
+                    <v-card-title
+                    >New Year Yoga - You Are Awesome!</v-card-title>
                     <v-card-text>
                       <video controls width="600" poster="https://herokustatic.s3.us-west-2.amazonaws.com/vid1_ss.png">
                         <source src="https://herokustatic.s3.us-west-2.amazonaws.com/NewYear.mp4" type="video/mp4" />
                       </video>
                     </v-card-text>
-                 </v-card>
-             </v-carousel-item>
-             <v-carousel-item>
-               <v-card color="transparent"
-                       elevation="10">
-                 <v-card-title
-                 >Deepening Through Grounding</v-card-title>
-                 <v-card-text>
-                   <video controls width="600" poster="https://herokustatic.s3.us-west-2.amazonaws.com/vid2_ss.png">
-                     <source src="https://herokustatic.s3.us-west-2.amazonaws.com/Grounding.mp4" type="video/mp4" />
-                   </video>
-                 </v-card-text>
-               </v-card>
-             </v-carousel-item>
-             <v-carousel-item>
-               <v-card color="transparent"
-                       elevation="10">
-                 <v-card-title
-                 >Daylight Savings Time (Yoga for Morning)</v-card-title>
-                 <v-card-text>
-                   <video controls width="600" poster="https://herokustatic.s3.us-west-2.amazonaws.com/vid3_ss.png">
-                     <source src="https://herokustatic.s3.us-west-2.amazonaws.com/Daylight_Savings.mp4" type="video/mp4" />
-                   </video>
-                 </v-card-text>
-               </v-card>
-             </v-carousel-item>
-             <v-carousel-item>
-               <v-card color="transparent"
-                       elevation="10">
-                 <v-card-title
-                 >Deepening Through Alignment</v-card-title>
-                 <v-card-text>
-                   <video controls width="600" poster="https://herokustatic.s3.us-west-2.amazonaws.com/vid4_ss.png">
-                     <source src="https://herokustatic.s3.us-west-2.amazonaws.com/Alignment.mp44" type="video/mp4" />
-                   </video>
-                 </v-card-text>
-               </v-card>
-             </v-carousel-item>
-           </v-carousel>
-         </v-container>
-    </v-card>
+                  </v-card>
+                </v-window-item>
+                <v-window-item>
+                  <v-card color="transparent"
+                          elevation="10">
+                    <v-card-title
+                    >Deepening Through Grounding</v-card-title>
+                    <v-card-text>
+                      <video controls width="600" poster="https://herokustatic.s3.us-west-2.amazonaws.com/vid2_ss.png">
+                        <source src="https://herokustatic.s3.us-west-2.amazonaws.com/Grounding.mp4" type="video/mp4" />
+                      </video>
+                    </v-card-text>
+                  </v-card>
+                </v-window-item>
+
+                <v-window-item>
+                  <v-card color="transparent"
+                          elevation="10">
+                    <v-card-title
+                    >Deepening Through Alignment</v-card-title>
+                    <v-card-text>
+                      <video controls width="600" poster="https://herokustatic.s3.us-west-2.amazonaws.com/vid4_ss.png">
+                        <source src="https://herokustatic.s3.us-west-2.amazonaws.com/Alignment.mp44" type="video/mp4" />
+                      </video>
+                    </v-card-text>
+                  </v-card>
+                </v-window-item>
+              </v-window>
+            </v-card>
+          </v-container>
+        </v-card>
+      </div>
+    </v-row>
+
+  </v-img>
+
   </div>
     </v-row>
+
   </v-container>
 
 </template>
